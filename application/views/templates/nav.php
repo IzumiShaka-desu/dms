cba27e5d4becc094d441a1aa205e62312dac2630
IzumiShaka-desu <shakaaji29@gmail.com>
2022-12-02 08:11:13 +0700

    hide unnecessary object

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -28,7 +28,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		<!-- Side Content -->
 		<div class="content-side">
 			<!-- Search -->
-			<div class="block pull-t pull-r-l">
+			<!-- <div class="block pull-t pull-r-l">
 				<div class="block-content block-content-full block-content-sm bg-body-light">
 					<form action="be_pages_generic_search.html" method="post">
 						<div class="input-group">
@@ -41,7 +41,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 						</div>
 					</form>
 				</div>
-			</div>
+			</div> -->
 			<!-- END Search -->
 
 			<!-- Mini Stats -->
@@ -362,7 +362,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 					</a>
 					<ul class="list-inline mt-10">
 						<li class="list-inline-item">
-							<a class="link-effect text-dual-primary-dark font-size-xs font-w600 text-uppercase" href="be_pages_generic_profile.html">J. Smith</a>
+							<a class="link-effect text-dual-primary-dark font-size-xs font-w600 text-uppercase" href="be_pages_generic_profile.html"><?php echo $_SESSION['username']; ?> </a>
 						</li>
 						<li class="list-inline-item">
 							<!-- Layout API, functionality initialized in Template._uiApiLayout() -->
@@ -371,7 +371,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 							</a>
 						</li>
 						<li class="list-inline-item">
-							<a class="link-effect text-dual-primary-dark" href="op_auth_signin.html">
+							<a class="link-effect text-dual-primary-dark" href="<?php echo base_url('login/logout'); ?> ">
 								<i class="si si-logout"></i>
 							</a>
 						</li>
@@ -424,14 +424,14 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 				<!-- Open Search Section -->
 				<!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-				<button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="header_search_on">
+				<!-- <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="header_search_on">
 					<i class="fa fa-search"></i>
-				</button>
+				</button> -->
 				<!-- END Open Search Section -->
 
 				<!-- Layout Options (used just for demonstration) -->
 				<!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-				<div class="btn-group" role="group">
+				<!-- <div class="btn-group" role="group">
 					<button type="button" class="btn btn-circle btn-dual-secondary" id="page-header-options-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<i class="fa fa-wrench"></i>
 					</button>
@@ -506,7 +506,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 				<!-- END Layout Options -->
 			</div>
 			<!-- END Left Section -->
@@ -517,116 +517,27 @@ defined('BASEPATH') or exit('No direct script access allowed');
 				<div class="btn-group" role="group">
 					<button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<i class="fa fa-user d-sm-none"></i>
-						<span class="d-none d-sm-inline-block">J. Smith</span>
+						<span class="d-none d-sm-inline-block"><?php echo $_SESSION['username']; ?></span>
 						<i class="fa fa-angle-down ml-5"></i>
 					</button>
-					<div class="dropdown-menu dropdown-menu-right min-width-200" aria-labelledby="page-header-user-dropdown">
-						<h5 class="h6 text-center py-10 mb-5 border-b text-uppercase">User</h5>
-						<a class="dropdown-item" href="be_pages_generic_profile.html">
-							<i class="si si-user mr-5"></i> Profile
-						</a>
-						<a class="dropdown-item d-flex align-items-center justify-content-between" href="be_pages_generic_inbox.html">
-							<span><i class="si si-envelope-open mr-5"></i> Inbox</span>
-							<span class="badge badge-primary">3</span>
-						</a>
-						<a class="dropdown-item" href="be_pages_generic_invoice.html">
-							<i class="si si-note mr-5"></i> Invoices
-						</a>
-						<div class="dropdown-divider"></div>
-
-						<!-- Toggle Side Overlay -->
-						<!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-						<a class="dropdown-item" href="javascript:void(0)" data-toggle="layout" data-action="side_overlay_toggle">
-							<i class="si si-wrench mr-5"></i> Settings
-						</a>
-						<!-- END Side Overlay -->
-
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item" href="op_auth_signin.html">
-							<i class="si si-logout mr-5"></i> Sign Out
-						</a>
-					</div>
 				</div>
 				<!-- END User Dropdown -->
 
 				<!-- Notifications -->
-				<div class="btn-group" role="group">
+				<!-- <div class="btn-group" role="group">
 					<button type="button" class="btn btn-rounded btn-dual-secondary" id="page-header-notifications" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<i class="fa fa-flag"></i>
 						<span class="badge badge-primary badge-pill">5</span>
 					</button>
-					<div class="dropdown-menu dropdown-menu-right min-width-300" aria-labelledby="page-header-notifications">
-						<h5 class="h6 text-center py-10 mb-0 border-b text-uppercase">Notifications</h5>
-						<ul class="list-unstyled my-20">
-							<li>
-								<a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-									<div class="ml-5 mr-15">
-										<i class="fa fa-fw fa-check text-success"></i>
-									</div>
-									<div class="media-body pr-10">
-										<p class="mb-0">You’ve upgraded to a VIP account successfully!</p>
-										<div class="text-muted font-size-sm font-italic">15 min ago</div>
-									</div>
-								</a>
-							</li>
-							<li>
-								<a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-									<div class="ml-5 mr-15">
-										<i class="fa fa-fw fa-exclamation-triangle text-warning"></i>
-									</div>
-									<div class="media-body pr-10">
-										<p class="mb-0">Please check your payment info since we can’t validate them!</p>
-										<div class="text-muted font-size-sm font-italic">50 min ago</div>
-									</div>
-								</a>
-							</li>
-							<li>
-								<a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-									<div class="ml-5 mr-15">
-										<i class="fa fa-fw fa-times text-danger"></i>
-									</div>
-									<div class="media-body pr-10">
-										<p class="mb-0">Web server stopped responding and it was automatically restarted!</p>
-										<div class="text-muted font-size-sm font-italic">4 hours ago</div>
-									</div>
-								</a>
-							</li>
-							<li>
-								<a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-									<div class="ml-5 mr-15">
-										<i class="fa fa-fw fa-exclamation-triangle text-warning"></i>
-									</div>
-									<div class="media-body pr-10">
-										<p class="mb-0">Please consider upgrading your plan. You are running out of space.</p>
-										<div class="text-muted font-size-sm font-italic">16 hours ago</div>
-									</div>
-								</a>
-							</li>
-							<li>
-								<a class="text-body-color-dark media mb-15" href="javascript:void(0)">
-									<div class="ml-5 mr-15">
-										<i class="fa fa-fw fa-plus text-primary"></i>
-									</div>
-									<div class="media-body pr-10">
-										<p class="mb-0">New purchases! +$250</p>
-										<div class="text-muted font-size-sm font-italic">1 day ago</div>
-									</div>
-								</a>
-							</li>
-						</ul>
-						<div class="dropdown-divider"></div>
-						<a class="dropdown-item text-center mb-0" href="javascript:void(0)">
-							<i class="fa fa-flag mr-5"></i> View All
-						</a>
-					</div>
-				</div>
+
+				</div> -->
 				<!-- END Notifications -->
 
 				<!-- Toggle Side Overlay -->
 				<!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-				<button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="side_overlay_toggle">
+				<!-- <button type="button" class="btn btn-circle btn-dual-secondary" data-toggle="layout" data-action="side_overlay_toggle">
 					<i class="fa fa-tasks"></i>
-				</button>
+				</button> -->
 				<!-- END Toggle Side Overlay -->
 			</div>
 			<!-- END Right Section -->
@@ -635,38 +546,19 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 		<!-- Header Search -->
 		<div id="page-header-search" class="overlay-header">
-			<div class="content-header content-header-fullrow">
-				<form action="be_pages_generic_search.html" method="post">
-					<div class="input-group">
-						<div class="input-group-prepend">
-							<!-- Close Search Section -->
-							<!-- Layout API, functionality initialized in Template._uiApiLayout() -->
-							<button type="button" class="btn btn-secondary" data-toggle="layout" data-action="header_search_off">
-								<i class="fa fa-times"></i>
-							</button>
-							<!-- END Close Search Section -->
-						</div>
-						<input type="text" class="form-control" placeholder="Search or hit ESC.." id="page-header-search-input" name="page-header-search-input">
-						<div class="input-group-append">
-							<button type="submit" class="btn btn-secondary">
-								<i class="fa fa-search"></i>
-							</button>
-						</div>
-					</div>
-				</form>
-			</div>
+
 		</div>
 		<!-- END Header Search -->
 
 		<!-- Header Loader -->
 		<!-- Please check out the Activity page under Elements category to see examples of showing/hiding it -->
-		<div id="page-header-loader" class="overlay-header bg-primary">
+		<!-- <div id="page-header-loader" class="overlay-header bg-primary">
 			<div class="content-header content-header-fullrow text-center">
 				<div class="content-header-item">
 					<i class="fa fa-sun-o fa-spin text-white"></i>
 				</div>
 			</div>
-		</div>
+		</div> -->
 		<!-- END Header Loader -->
 	</header>
 	<!-- END Header -->
