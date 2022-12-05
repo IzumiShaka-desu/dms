f8dc91a2f57478fe728aaac35028845ab1d69ce5
IzumiShaka-desu <shakaaji29@gmail.com>
2022-12-05 08:07:36 +0700

    fix: bugs NAN date

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -269,26 +269,37 @@
 							newobj[key.toLowerCase().trim().replace(" ", "_")] = obj[key].toString();
 
 						}
-						var serial = newobj['expired_date'];
-						// newobj['expired_date'] = newobj['__empty_1'] + "/" + newobj['__empty'] + "/" + newobj['expired'];
-						var utc_days = Math.floor(serial - 25569);
-						var utc_value = utc_days * 86400;
-						var date_info = new Date(utc_value * 1000);
+						// check if is expired date is nan or not
+						if (isNaN(newobj.expired_date)) {
+							newobj.expired_date = newobj.expired_date;
+						} else {
+							var serial = newobj['expired_date'];
+							// newobj['expired_date'] = newobj['__empty_1'] + "/" + newobj['__empty'] + "/" + newobj['expired'];
+							var utc_days = Math.floor(serial - 25569);
+							var utc_value = utc_days * 86400;
+							var date_info = new Date(utc_value * 1000);
 
-						var fractional_day = serial - Math.floor(serial) + 0.0000001;
+							var fractional_day = serial - Math.floor(serial) + 0.0000001;
 
-						var total_seconds = Math.floor(86400 * fractional_day);
+							var total_seconds = Math.floor(86400 * fractional_day);
 
-						var seconds = total_seconds % 60;
+							var seconds = total_seconds % 60;
 
-						total_seconds -= seconds;
+							total_seconds -= seconds;
 
-						var hours = Math.floor(total_seconds / (60 * 60));
-						var minutes = Math.floor(total_seconds / 60) % 60;
+							var hours = Math.floor(total_seconds / (60 * 60));
+							var minutes = Math.floor(total_seconds / 60) % 60;
 
-						var date = new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
-						// assign date as yyyy/mm/dd string to newobj expired_date
-						newobj['expired_date'] = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+							var date = new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+							// assign date as yyyy/mm/dd string to newobj expired_date
+							newobj['expired_date'] = date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
+
+						}
+						// string of yyyy/mm/dd to date
+						var expired_date = newobj['expired_date'];
+						var date = new Date(expired_date);
+
+
 
 						// var date = new Date(newobj['expired_date']);
 						newobj['date'] = date;
